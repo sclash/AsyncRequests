e4dd2ee1279e2319b48918c1d5ffcabe232957bc
sclash <andrea.sergi1@gmail.com>
2024-01-16 18:36:55 +0100

    bug fix

--- src/RequestObject.py
+++ src/RequestObject.py
@@ -19,10 +19,4 @@ class RequestObject:
     stream:bool=None
     verify:bool = True
     cert:str | tuple = None
-<<<<<<< HEAD
     json:dict=None
-=======
-    json:dict=None
-    status: int = None
-    request_error: str = None
->>>>>>> 5996166 (performance imporvement - unsuccessful requests handling - progressbar tracking)
